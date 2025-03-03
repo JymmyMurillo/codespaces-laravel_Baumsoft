42d648191cf84a7eec97061939d2a0438836f483
Jymmy Murillo <murillojymmy@gmail.com>
2025-03-03 03:28:15 +0000

    Implementación de ruta para crear un ingreso

--- app/Http/Controllers/IngresoController.php
+++ app/Http/Controllers/IngresoController.php
@@ -6,5 +6,20 @@ use Illuminate\Http\Request;
 
 class IngresoController extends Controller
 {
-    //
+    public function crear(Request $request, $usuario_id)
+    {
+        $usuario = Usuario::findOrFail($usuario_id);
+
+        $ingreso = Ingreso::create([
+            'usuario_id' => $usuario_id,
+            'fecha_entrada' => now(),
+            'fecha_salida' => now()->addHours(5),
+        ]);
+
+        return response()->json([
+            'message' => 'Ingreso creado',
+            'ingreso' => $ingreso,
+            'usuario' => $usuario
+        ], 201);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,6 @@ Route::get('/usuarios/modificar/{id}', [UsuarioController::class, 'modificar']);
 Route::get('/usuarios/eliminar/{id}', [UsuarioController::class, 'eliminar']);
 
 Route::get('/usuarios/consultar/{id}', [UsuarioController::class, 'consultar']);
+
+
+Route::get('/ingresos/crear/{usuario_id}', [IngresoController::class, 'crear']);
