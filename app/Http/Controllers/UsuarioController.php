290e12f8768e48f00bcf5b62f4af06f5dced982d
Jymmy Murillo <murillojymmy@gmail.com>
2025-03-03 03:19:12 +0000

    Implementación de rutas para crear y modificar un usuario

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UsuarioController extends Controller
+{
+    public function crear(Request $request)
+    {
+        $request->validate([
+            'correo' => 'required|email|unique:usuarios,correo',
+            'nombres' => 'required',
+            'apellidos' => 'required',
+        ]);
+
+        $usuario = Usuario::create($request->all());
+
+        return response()->json([
+            'message' => 'Usuario creado',
+            'usuario' => $usuario
+        ], 201);
+    }
+
+    public function modificar(Request $request, $id)
+    {
+        $usuario = Usuario::findOrFail($id);
+
+        $request->validate([
+            'correo' => 'email|unique:usuarios,correo,' . $usuario->id,
+            'nombres' => 'required',
+            'apellidos' => 'required',
+        ]);
+
+        $usuario->update($request->all());
+
+        return response()->json([
+            'message' => 'Usuario actualizado',
+            'usuario' => $usuario
+        ], 200);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/usuarios/crear', [UsuarioController::class, 'crear']);
+
+Route::get('/usuarios/modificar/{id}', [UsuarioController::class, 'modificar']);
