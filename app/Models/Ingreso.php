8764ad1159177ba83f03e571ce4392f9b6b09ead
Jymmy Murillo <murillojymmy@gmail.com>
2025-03-03 03:06:01 +0000

    Definición de relaciones en los modelos Usuario e Ingreso

--- app/Models/Ingreso.php
+++ app/Models/Ingreso.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Ingreso extends Model
 {
-    use HasFactory;
+    protected $fillable = ['usuario_id', 'fecha_entrada', 'fecha_salida'];
+
+    public function usuario()
+    {
+        return $this->belongsTo(Usuario::class);
+    }
 }
